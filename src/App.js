5ce9d6c715afdcfd191d8048cb48af55fbd20a14
BenjaminKyhn <benj2605@edu.easj.dk>
2021-08-17 11:00:58 +0200

    Changed project structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
 import {useTranslation, Trans, initReactI18next} from "react-i18next";
 import React, { useState, Suspense } from "react";
-import i18n from "i18next";
-import ticketing_da from "./language/da/ticketing.json";
-import ticketing_en from "./language/en/ticketing.json";
-
-i18n
-    .use(initReactI18next)
-    .init({
-        resources: {
-            en: { translation: ticketing_en },
-            da: { translation: ticketing_da },
-        },
-        lng: "en",
-        fallbackLng: "en",
-        interpolation: { escapeValue: false },
-    });
+import i18next from "i18next";
 
 const App = () => {
     const { t } = useTranslation();
     const [count, setCount] = useState(0);
     const onChange = (event) => {
-        i18n.changeLanguage(event.target.value);
+        i18next.changeLanguage(event.target.value);
         setCount((previousCount) => previousCount + 1);
     };
     return (
--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from "i18next";
+import {initReactI18next, I18nextProvider} from "react-i18next";
+import ticketing_en from "./language/en/ticketing.json";
+import ticketing_da from "./language/da/ticketing.json";
+
+i18next
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {translation: ticketing_en},
+            da: {translation: ticketing_da},
+        },
+        lng: "en",
+        fallbackLng: "en",
+        interpolation: {escapeValue: false},
+    });
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <I18nextProvider i18n={i18next}>
+            <App/>
+        </I18nextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
